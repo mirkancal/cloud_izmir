b34cf5dae9525a53d1cc24515dd9d6045af67075
Mirkan <mirkancaliskan@gmail.com>
2020-02-02 13:34:08 +0300

    first changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_izmir/pages/welcome_page.dart';
+import 'package:cloud_izmir/services/user_service.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,12 +7,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.green,
+        brightness: Brightness.dark,
       ),
-      home: WelcomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final userService = UserService();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: ListView(
+          children: <Widget>[],
+        ),
+      ),
     );
   }
 }
---
+++ lib/services/user_service.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_izmir/models/user.dart';
+import 'package:dio/dio.dart';
+
+class UserService {
+  final client = Dio();
+
+  Future<List<User>> getUsers() async {
+    final response =
+        await client.get("https://jsonplaceholder.typicode.com/users");
+
+    final users = (response.data as List).map((item) {
+      return User.fromJson(item);
+    }).toList();
+    return users;
+  }
+}
